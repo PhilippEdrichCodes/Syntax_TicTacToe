ae923c95ef7777afb2c56f55aa0b1c9b90c8fe36
PhilippEdrichCodes <103415239+PhilippEdrichCodes@users.noreply.github.com>
2022-04-27 17:50:21 +0200

    Add TicTacToeAdvanced.js Changed Output-Format Additional Comments

--- TicTacToeAdvanced.js
+++ TicTacToeAdvanced.js
@@ -64,8 +64,21 @@ let ticTac = [["_","_","_"],["_","_","_"],["_","_","_"]]
  * Damit kann ich fürs erste Leben. Aber damit ich die einzelnen Züge auf der
  * Konsole auch auf den ersten Blick unterscheiden kann, füge ich noch
  * gleich an die Ausgabe eine weitere leere neue Zeile an:
- * console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
+ * console.debug(ticTac.join("\n") + "\n")
  *
+ * Jetzt sollen auch noch die Kommas weg.
+ * Deshalb nehme ich nun das join() nicht für das gesamte Array, sondern nur für die inneren.
+ * Die Zeichen sollen aber nicht jedes in einer einzelnen Zeile stehen, sondern hintereinander.
+ * Deshalb nutze ich jetzt als Trenner nicht mehr "\n" sondern das Leerzeichen " ":
+ * ticTac[0].join(" ")
+ *
+ * Das gibt natürlich nur die erste Zeile zurück:
+ *  _ _ _
+ *
+ * Deshalb das gleiche nochmal für die anderen beiden Zeilen. Diese verbinde ich anschließend wieder
+ * mit \n und gebe das ganze dann mit console.debug() aus. Das zusätzliche \n am Ende behalte ich
+ * natürlich auch weiterhin bei:
+ * console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
 
@@ -77,26 +90,28 @@ ticTac[0][1] = "x"
 
 /*
  * Ergibt:
- * _,x,_
- * _,_,_
- * _,_,_
+ * _ x _
+ * _ _ _
+ * _ _ _
  *
  * Die gesamte Ausgabe sieht jetzt, durch das zusätzliche \n, aus wie folgt:
- * _,_,_
- * _,_,_
- * _,_,_
+ * _ _ _
+ * _ _ _
+ * _ _ _
  *
- * _,x,_
- * _,_,_
- * _,_,_
+ * _ x _
+ * _ _ _
+ * _ _ _
  *
  * Ohne würde es so aussehen:
- * _,_,_
- * _,_,_
- * _,_,_
- * _,x,_
- * _,_,_
- * _,_,_
+ * _ _ _
+ * _ _ _
+ * _ _ _
+ * _ x _
+ * _ _ _
+ * _ _ _
+ *
+ * Hier sind die einzelnen Spielzüge nicht zu unterscheiden
  *
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
@@ -109,9 +124,9 @@ ticTac[0][0] = "o"
 
 /*
  * Ausgabe:
- * o,x,_
- * _,_,_
- * _,_,_
+ * o x _
+ * _ _ _
+ * _ _ _
  *
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
@@ -125,9 +140,9 @@ ticTac[2][1] = "x"
 
 /*
  * Ausgabe
- * o,x,_
- * _,_,_
- * _,x,_
+ * o x _
+ * _ _ _
+ * _ x _
  *
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
@@ -141,9 +156,9 @@ ticTac[1][1] = "o"
 
 /*
  * Ausgabe:
- * o,x,_
- * _,o,_
- * _,x,_
+ * o x _
+ * _ o _
+ * _ x _
  *
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
@@ -157,9 +172,9 @@ ticTac[2][2] = "x"
 
 /*
  * Ausgabe:
- * o,x,_
- * _,o,_
- * _,x,x
+ * o x _
+ * _ o _
+ * _ x x
  *
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
@@ -173,9 +188,9 @@ ticTac[2][0] = "o"
 
 /*
  * Ausgabe:
- * o,x,_
- * _,o,_
- * o,x,x
+ * o x _
+ * _ o _
+ * o x x
  *
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
@@ -189,9 +204,9 @@ ticTac[1][0] = "x"
 
 /*
  * Ausgabe:
- * o,x,_
- * x,o,_
- * o,x,x
+ * o x _
+ * x o _
+ * o x x
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
 
@@ -203,9 +218,9 @@ ticTac[0][2] = "o"
 
 /*
  * Ausgabe:
- * o,x,o
- * x,o,_
- * o,x,x
+ * o x o
+ * x o _
+ * o x x
  */
 console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2].join(" ") + "\n" )
 
@@ -213,44 +228,48 @@ console.debug(ticTac[0].join(" ") + "\n" + ticTac[1].join(" ") + "\n" + ticTac[2
 console.debug("o wins!")
 
 /*
-Die gesamte Ausgabe des Games sieht folgendermaßen aus:
 
-_,_,_
-_,_,_
-_,_,_
+Die gesamte Ausgabe des Spiels sieht dann folgendermaßen aus:
 
-_,x,_
-_,_,_
-_,_,_
 
-o,x,_
-_,_,_
-_,_,_
+_ _ _
+_ _ _
+_ _ _
 
-o,x,_
-_,_,_
-_,x,_
+_ x _
+_ _ _
+_ _ _
 
-o,x,_
-_,o,_
-_,x,_
+o x _
+_ _ _
+_ _ _
 
-o,x,_
-_,o,_
-_,x,x
+o x _
+_ _ _
+_ x _
 
-o,x,_
-_,o,_
-o,x,x
+o x _
+_ o _
+_ x _
 
-o,x,_
-x,o,_
-o,x,x
+o x _
+_ o _
+_ x x
 
-o,x,o
-x,o,_
-o,x,x
+o x _
+_ o _
+o x x
+
+o x _
+x o _
+o x x
+
+o x o
+x o _
+o x x
 
 o wins!
 
-Process finished with exit code 0*/
+Process finished with exit code 0
+
+*/
